a34b43a276b425886e542086fe65822d60ab87e6
jiwonniy <jiwoni0707@gmail.com>
2024-07-22 00:04:09 +0900

    refactor: OpenApiService 인터페이스를 상속받도록 변경

--- src/main/java/com/godseven/muntour/openapi/service/PerformanceService.java
+++ src/main/java/com/godseven/muntour/openapi/service/PerformanceService.java
@@ -3,71 +3,49 @@ package com.godseven.muntour.openapi.service;
 import com.godseven.muntour.openapi.config.PerformanceConst;
 import com.godseven.muntour.openapi.dto.response.PerformanceResponse;
 import com.godseven.muntour.openapi.dto.response.PerformanceResponseList;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Slf4j
-public class PerformanceService {
+public class PerformanceService implements OpenApiService<PerformanceResponse>{
 
-    private String makePerformanceUrl(int pageNo) {
-        String baseUrl = PerformanceConst.ENDPOINT
-                + PerformanceConst.getServiceKey()
-                + PerformanceConst.NUM_OF_ROWS
-                + PerformanceConst.PAGE_NO;
-        String pageNoStr = String.valueOf(pageNo);
-        return baseUrl + pageNoStr;
-    }
+    private final OpenApiServiceUtil openApiServiceUtil;
 
+    /**
+     * 공연 목록 불러오기
+     */
     public PerformanceResponseList getPerformanceList(int pageNo) {
-        String apiUrl = makePerformanceUrl(pageNo);
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "application/json");
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
-
-        List<PerformanceResponse> performanceResponses = new ArrayList<>();
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            JSONParser parser = new JSONParser();
-            try {
-                JSONObject jsonResponse = (JSONObject) parser.parse(response.getBody());
-                JSONObject responseObj = (JSONObject) jsonResponse.get("response");
-                JSONObject body = (JSONObject) responseObj.get("body");
-                JSONObject items = (JSONObject) body.get("items");
-                JSONArray itemArray = (JSONArray) items.get("item");
-
-                for (Object obj : itemArray) {
-                    JSONObject item = (JSONObject) obj;
-                    String title = (String) item.get("TITLE");
-                    String imageObject = (String) item.get("IMAGE_OBJECT");
-                    String url = (String) item.get("URL");
-                    String period = (String) item.get("PERIOD");
-                    performanceResponses.add(
-                            PerformanceResponse.builder()
-                                    .title(title)
-                                    .imageObject(imageObject)
-                                    .url(url)
-                                    .period(period).build()
-                            );
-                }
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
+        List<PerformanceResponse> performanceResponses = fetchData(pageNo);
         return PerformanceResponseList.from(performanceResponses);
     }
 
+    @Override
+    public String makeUrl(int pageNo) {
+        String url = PerformanceConst.ENDPOINT
+                + PerformanceConst.getServiceKey()
+                + PerformanceConst.NUM_OF_ROWS
+                + PerformanceConst.PAGE_NO
+                + pageNo;
+        return url;
+    }
+
+    @Override
+    public List<PerformanceResponse> fetchData(int pageNo) {
+        String apiUrl = makeUrl(pageNo);
+        return openApiServiceUtil.fetchData(apiUrl, item ->
+                PerformanceResponse.builder()
+                        .title((String) item.get("TITLE"))
+                        .imageObject((String) item.get("IMAGE_OBJECT"))
+                        .url((String) item.get("URL"))
+                        .period((String) item.get("PERIOD"))
+                        .build()
+        );
+    }
 }
