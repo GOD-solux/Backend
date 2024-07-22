ca7454f6bbdcec626fd8f41086cda81dd30665bc
jiwonniy <jiwoni0707@gmail.com>
2024-07-23 00:05:55 +0900

    feat: 문화생활더보기 api

---
+++ src/main/java/com/godseven/muntour/openapi/controller/OpenApiController.java
@@ -0,0 +1,59 @@
+package com.godseven.muntour.openapi.controller;
+
+import com.godseven.muntour.openapi.dto.response.BookResponseList;
+import com.godseven.muntour.openapi.dto.response.ExhibitionResponseList;
+import com.godseven.muntour.openapi.dto.response.PerformanceResponseList;
+import com.godseven.muntour.openapi.service.BookService;
+import com.godseven.muntour.openapi.service.ExhibitionService;
+import com.godseven.muntour.global.dto.ResponseTemplate;
+import com.godseven.muntour.openapi.service.PerformanceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "OpenApiController", description = "문화 생활 더보기 API (전시, 공연, 도서, 스포츠)")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/muntour/open-api")
+public class OpenApiController {
+
+    private final ExhibitionService exhibitionService;
+    private final PerformanceService performanceService;
+    private final BookService bookService;
+
+    @Operation(summary = "전시회 목록보기", description = "전시회 목록보기")
+    @GetMapping("/exhibition")
+    public ResponseEntity<ResponseTemplate<Object>> getExhibitionList(@RequestParam(defaultValue = "1") int pageNo) {
+
+        ExhibitionResponseList responses = exhibitionService.getExhibitionList(pageNo);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseTemplate.from(responses));
+    }
+
+    @Operation(summary = "공연 목록보기", description = "공연 목록보기")
+    @GetMapping("/performance")
+    public ResponseEntity<ResponseTemplate<Object>> getPerformanceList(@RequestParam(defaultValue = "1") int pageNo) {
+
+        PerformanceResponseList responses = performanceService.getPerformanceList(pageNo);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseTemplate.from(responses));
+    }
+
+    @Operation(summary = "추천 도서 목록보기", description = "추천 도서 목록보기")
+    @GetMapping("/book")
+    public ResponseEntity<ResponseTemplate<Object>> getBookList(@RequestParam(defaultValue = "1") int pageNo) {
+
+        BookResponseList responses = bookService.getBookList(pageNo);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseTemplate.from(responses));
+    }
+}
