b06e0c9959ad029a3b2ac5007b1e19f126a7d5d8
hasol11 <kangji09879@sookmyung.ac.kr>
2024-07-28 00:07:31 +0900

    feat: 게시글, 댓글, 해시태그 구현

--- src/main/java/com/godseven/muntour/post/repository/HashTagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.godseven.muntour.post.repository;
-
-import com.godseven.muntour.post.entity.Hashtag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HashTagRepository extends JpaRepository<Hashtag, Long> {
-}
--- src/main/java/com/godseven/muntour/post/repository/TagMappingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.godseven.muntour.post.repository;
-
-import com.godseven.muntour.post.entity.TagMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagMappingRepository extends JpaRepository<TagMapping, Long> {
-}
